9bdd3187f0da8f71c115f140c98ed8cdd9f7a9d9
Moni <monica_be@hotmail.fr>
2020-05-04 17:25:53 +0200

    adding axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios;'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import FormMovie from './components/Form'
 
 
 
-function App() {
+class App extends Component {
+
+  
+
+  render(){
   return (
     <div className="App">
     <FormMovie />
 
     </div>
-  );
+  )
+  }
 }
 
 export default App;
