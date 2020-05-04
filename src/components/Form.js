6d3f862a08c6bd7df7f0fce8aa9d1dc0ab85749b
Moni <monica_be@hotmail.fr>
2020-05-04 11:44:50 +0200

    initilizing + bootstrap

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Movie from './MovieFile'
+
+class FormMovie extends Component {
+    render(){
+        return(
+
+
+            <Movie />
+            
+        )
+    }
+
+}
+
+export default FormMovie
---
+++ src/components/MovieFile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+function Movie (){
+    return(
+    <div>
+        <h3>Title</h3>
+        <img src="" />
+        <p>comment</p>
+    </div>
+    ) 
+}
+
+
+export default Movie
